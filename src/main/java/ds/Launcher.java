3dcb393eead411ded8160a702fb6294941bbba95
pseusys <shveitsar215@gmail.com>
2023-03-27 15:37:09 +0200

    physical node initialization

--- src/main/java/ds/Launcher.java
+++ src/main/java/ds/Launcher.java
@@ -2,10 +2,9 @@ package main.java.ds;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class Launcher {
-    private int[][] matrix = {
+    private static int[][] matrix = {
             {-1, -1, 1, -1, -1},
             {-1, -1, 1, 1, 1},
             {1, 1, -1, -1, -1},
@@ -13,21 +12,11 @@ public class Launcher {
             {-1, 1, -1, -1, -1}
     };
 
-    private Map<PhysicalNode, VirtualNode> map = Map.of(
-            new PhysicalNode(0), new VirtualNode(0),
-            new PhysicalNode(1), new VirtualNode(1),
-            new PhysicalNode(2), new VirtualNode(2),
-            new PhysicalNode(3), new VirtualNode(3),
-            new PhysicalNode(4), new VirtualNode(4)
-    );
-
     public static void main(String[] args) {
         List<PhysicalNode> physical = new ArrayList<>();
 
         for (int i = 0; i < 5; i++) {
-            physical.add(new PhysicalNode(i));
+            physical.add(new PhysicalNode(i, 5, matrix[i]));
         }
-
-
     }
 }
--- src/main/java/ds/PhysicalNode.java
+++ src/main/java/ds/PhysicalNode.java
@@ -1,7 +1,22 @@
 package main.java.ds;
 
 public class PhysicalNode extends Node {
-    public PhysicalNode(int id) {
+    int nodesNumber;
+
+    int[] neighbors;
+
+    int[][] routingTable;
+
+    public PhysicalNode(int id, int nodesNumber, int[] neighbors) {
         this.id = id;
+        this.nodesNumber = nodesNumber;
+        this.neighbors = neighbors;
+        this.routingTable = new int[nodesNumber][3];
+        for (int i = 0; i < nodesNumber; i++) {
+            if (i == id) routingTable[i] = new int[] {id, id, 0};
+            else routingTable[i] = new int[] {-1, i, Integer.MAX_VALUE};
+        }
     }
+
+
 }
